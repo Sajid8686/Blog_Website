7ba43baf2abe4fefa8c63449098385dccec218cf
Sajid Hussain <sajid6207116@gmail.com>
2024-06-25 19:49:48 +0500

    Seo friendly Urls

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
 
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -27,6 +27,7 @@ class Post(models.Model):
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     objects = models.Manager()
     published = PublishedManager()
+    
     
     class Meta:
         ordering = ['-publish']
@@ -40,5 +41,5 @@ class Post(models.Model):
     
     
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.id])
+        return reverse("blog:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
     
