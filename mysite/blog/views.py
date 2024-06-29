a9a31ada4ae70b7204cc348e655df359dd4e34d3
Sajid Hussain <sajid6207116@gmail.com>
2024-06-29 18:06:34 +0500

    similar posts section complete

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 def post_list(request, tag_slug=None):
@@ -29,7 +30,14 @@ def post_detail(request, year, month, day, post):
     
     comments = post.comments.filter(active=True)
     form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post':post, 'comments':comments, 'form':form})  
+    
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
+    
+    return render(request, 'blog/post/detail.html', {'post':post, 'comments':comments, 'form':form, 'similar_posts': similar_posts})  
 
 
 def post_share(request, post_id):
